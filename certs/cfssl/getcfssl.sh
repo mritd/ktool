148f6a18f1416771ce8b9b741cafcb782683ec6a
mritd <mritd1234@gmail.com>
2017-12-28 15:27:34 +0800

    :bug:fix(0.0.1): 修复 cfssl 安装路径错误
    修复 cfssl 安装路径错误
    
    Signed-off-by: mritd <mritd1234@gmail.com>

--- certs/cfssl/getcfssl.sh
+++ certs/cfssl/getcfssl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget https://mritdftp.b0.upaiyun.com/cfssl/cfssl -O /usr/local/cfssl
-wget https://mritdftp.b0.upaiyun.com/cfssl/cfssljson -O /usr/local/cfssljson
+wget https://mritdftp.b0.upaiyun.com/cfssl/cfssl -O /usr/local/bin/cfssl
+wget https://mritdftp.b0.upaiyun.com/cfssl/cfssljson -O /usr/local/bin/cfssljson
 
-chmod +x /usr/local/cfssl /usr/local/cfssljson
+chmod +x /usr/local/bin/cfssl /usr/local/bin/cfssljson
