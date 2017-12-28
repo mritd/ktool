31013f6be30f1f1c489909b1cf6f8a367576d8eb
mritd <mritd1234@gmail.com>
2017-12-28 15:41:18 +0800

    :sparkles: feat(0.0.1): 使用压缩文件
    使用压缩文件
    
    Signed-off-by: mritd <mritd1234@gmail.com>

--- certs/cfssl/getcfssl.sh
+++ certs/cfssl/getcfssl.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-wget https://mritdftp.b0.upaiyun.com/cfssl/cfssl -O /usr/local/bin/cfssl
-wget https://mritdftp.b0.upaiyun.com/cfssl/cfssljson -O /usr/local/bin/cfssljson
-
+wget https://mritdftp.b0.upaiyun.com/cfssl/cfssl.tar.gz
+tar -zxvf cfssl.tar.gz
+mv cfssl cfssljson /usr/local/bin
 chmod +x /usr/local/bin/cfssl /usr/local/bin/cfssljson
+rm -f cfssl.tar.gz
