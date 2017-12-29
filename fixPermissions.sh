3af84ccc9206ee21c32134c7ec83eb79e82edca1
mritd <mritd1234@gmail.com>
2017-12-29 13:02:24 +0800

    :sparkles: feat(0.0.1): 增加目录判断
    增加目录判断
    
    Signed-off-by: mritd <mritd1234@gmail.com>

--- fixPermissions.sh
+++ fixPermissions.sh
@@ -1,7 +1,7 @@
 #/!bin/bash
 
-chmod -R 755 /etc/etcd/ssl
-chown -R etcd:etcd /etc/etcd/ssl
-chown -R etcd:etcd /var/lib/etcd
-chown -R kube:kube /var/log/kube-audit
-chown -R kube:kube /etc/kubernetes/ssl
+[ -d "/etc/etcd/ssl" ] && chmod -R 755 /etc/etcd/ssl
+[ -d "/etc/etcd/ssl" ] && chown -R etcd:etcd /etc/etcd/ssl
+[ -d "/var/lib/etcd" ] && chown -R etcd:etcd /var/lib/etcd
+[ -d "/var/log/kube-audit" ] && chown -R kube:kube /var/log/kube-audit
+[ -d "/etc/kubernetes/ssl" ] && chown -R kube:kube /etc/kubernetes/ssl
