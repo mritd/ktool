1a09f6a571577f0e0ed41fd8dd74f899b4b6beb0
mritd <mritd1234@gmail.com>
2017-12-29 12:43:17 +0800

    :sparkles: feat(0.0.1): 增加复制脚本
    增加复制脚本
    
    Signed-off-by: mritd <mritd1234@gmail.com>

---
+++ certs/kubernetes/cpConfig.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rm -rf ../../k8s/conf/ssl
+rm -f ../../k8s/conf/*.csv
+rm -f ../../k8s/conf/*.yaml
+rm -f ../../k8s/conf/*.kubeconfig
+
+mkdir ../../k8s/conf/ssl
+cp *.pem ../../k8s/conf/ssl
+cp *.csv ../../k8s/conf
+cp *.yaml ../../k8s/conf
+cp *.kubeconfig ../../k8s/conf
