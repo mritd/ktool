a7fefed39a3aa81870caf586086ee975425c75eb
mritd <mritd1234@gmail.com>
2017-12-31 16:32:47 +0800

    :sparkles: feat(0.0.1): 增加 /var/lib/kubelet 权限修复
    增加 /var/lib/kubelet 权限修复
    
    Signed-off-by: mritd <mritd1234@gmail.com>

--- fixPermissions.sh
+++ fixPermissions.sh
@@ -4,4 +4,5 @@
 [ -d "/etc/etcd/ssl" ] && chown -R etcd:etcd /etc/etcd/ssl
 [ -d "/var/lib/etcd" ] && chown -R etcd:etcd /var/lib/etcd
 [ -d "/var/log/kube-audit" ] && chown -R kube:kube /var/log/kube-audit
+[ -d "/var/lib/kubelet" ] && chown -R kube:kube /var/lib/kubelet
 [ -d "/etc/kubernetes/ssl" ] && chown -R kube:kube /etc/kubernetes/ssl
