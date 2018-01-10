a927067d707cd00616378fb512f3645b66c9670b
mritd <mritd1234@gimail.com>
2018-01-10 15:12:56 +0800

    :sparkles: feat(0.0.1): 调整目录名，合并脚本
    调整目录名，合并脚本
    
    Signed-off-by: mritd <mritd1234@gimail.com>

--- certs/kubernetes/genCerts.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cfssl gencert --initca=true k8s-root-ca-csr.json | cfssljson --bare k8s-root-ca
-
-for targetName in kubernetes admin kube-proxy; do
-    cfssl gencert --ca k8s-root-ca.pem --ca-key k8s-root-ca-key.pem --config k8s-gencert.json --profile kubernetes $targetName-csr.json | cfssljson --bare $targetName
-done
